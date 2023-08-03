ad1bbdda88051af1ab2e2caededbeb90a045fe7a
ShabdaliSuchitraSanjayJagtap1214 <140742495+ShabdaliSuchitraSanjayJagtap1214@users.noreply.github.com>
2023-08-03 18:33:36 +0530

    Create Select Command :- Used to Display Records from Table.sql

---
+++ 5. DQL (Data Query Language) Commands :- It Retrieve Data from the DBMS using SQL Queries/Select Command :- Used to Display Records from Table.sql
@@ -0,0 +1,7 @@
+## Select Command :- Used to Display Records from Table
+
+-- a.display all records
+select * from customer;
+
+-- b.display particular columns
+select customerid, customernumber, lastname, firstname from customer;
