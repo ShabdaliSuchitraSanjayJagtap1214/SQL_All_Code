704038b52faeb53abeff5333a50e44ac27b75959
ShabdaliSuchitraSanjayJagtap1214 <140742495+ShabdaliSuchitraSanjayJagtap1214@users.noreply.github.com>
2023-08-03 16:11:59 +0530

    Create a. Insert Command :- Inserting values into the table Using Insert Command.sql

---
+++ 4. DML (Data Manipulation Language) Commands :- It Manipulate Data Present in the DBMS/a. Insert Command :- Inserting values into the table Using Insert Command.sql
@@ -0,0 +1,6 @@
+## Insert Command :- Inserting values into the table Using Insert Command
+
+insert into customer values
+(1,100,'Fang Ying','Sham','418999','sdadasfdfd',default),
+(2,200,'Mei Mei','Tan',default,'adssdsadsd','Thailand'),
+(4,300,'Albert','John',default,'dfdsfsdf',default);
